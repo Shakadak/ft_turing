0caddd07b37c17fe01ef63308a4a60375e706a6b
Nathanael PINEAU <npineau@student.42.fr>
2016-02-09 16:21:27 +0100

    untested

--- turing/app/Main.hs
+++ turing/app/Main.hs
@@ -3,9 +3,13 @@ module Main where
 import System.Environment
 import Lib
 import Machine
+import qualified Data.ByteString.Lazy.Char8 as B
+import Data.Aeson.Generic
 
 main :: IO ()
 main = do
     args <- getArgs
-    content <- readFile (head args)
+    content <- readFile (Prelude.head args)
     putStr content
+    putStrLn "-----"
+    putStrLn . show $ (decode $ B.pack content :: Maybe Machine)
